e094f88cb61bb170d289ed9260d79cd9737a1eab
Дарья Торгова <dtorgova@gmail.com>
2025-12-07 22:18:10 +0300

    Added implementations of heap, merge, quich sorts. Built tests for heap sort.

---
+++ src/tests_pytest_6/quick_sort.py
@@ -0,0 +1,65 @@
+def partition(array, start, end):
+    pivot = array[start]
+    low = start + 1
+    high = end
+
+    while True:
+        while low <= high and array[high] >= pivot:
+            high = high - 1
+
+        while low <= high and array[low] <= pivot:
+            low = low + 1
+
+        if low <= high:
+            array[low], array[high] = array[high], array[low]
+        else:
+            break
+
+    array[start], array[high] = array[high], array[start]
+
+    return high
+
+
+def quick_sort(array, start=0, end=None):
+    if end is None:
+        end = len(array) - 1
+
+    if start >= end:
+        return array
+
+    p = partition(array, start, end)
+    quick_sort(array, start, p - 1)
+    quick_sort(array, p + 1, end)
+    return array
+
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+    return arr
