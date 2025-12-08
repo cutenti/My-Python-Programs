b2d68bee5b46a5a3bf393ce83ad72983428dd8e2
Daria <dtorgova@gmail.com>
2025-12-08 19:49:49 +0300

    Update quick_sort.py
    Забыла изначально удалить функцию merge_sort

--- src/tests_pytest_6/quick_sort.py
+++ src/tests_pytest_6/quick_sort.py
@@ -31,35 +31,3 @@ def quick_sort(array, start=0, end=None):
     quick_sort(array, start, p - 1)
     quick_sort(array, p + 1, end)
     return array
-
-
-def merge_sort(arr):
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        left = arr[:mid]
-        right = arr[mid:]
-
-        merge_sort(left)
-        merge_sort(right)
-
-        i = j = k = 0
-
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                arr[k] = left[i]
-                i += 1
-            else:
-                arr[k] = right[j]
-                j += 1
-            k += 1
-
-        while i < len(left):
-            arr[k] = left[i]
-            i += 1
-            k += 1
-
-        while j < len(right):
-            arr[k] = right[j]
-            j += 1
-            k += 1
-    return arr
